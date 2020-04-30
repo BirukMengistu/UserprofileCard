6f5c201d4e33272c65165c1c8b3e4f99c7772d45
BirukMengistu <sheger.shelf3@gmail.com>
2020-04-30 15:30:29 +0200

    Updates

---
+++ precache-manifest.20090027da6d30c8c484a9ec4c1ba4b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e260406ff2095bf3a4c30eb7a7665c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91b637a8efe919c1dcf",
+    "url": "/static/css/main.34d8e25d.chunk.css"
+  },
+  {
+    "revision": "391f0135aea4d9dbd9fd",
+    "url": "/static/js/2.4ffc9a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ffc9a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91b637a8efe919c1dcf",
+    "url": "/static/js/main.8f9b06ad.chunk.js"
+  },
+  {
+    "revision": "bdcbbeffaf130f4e244d",
+    "url": "/static/js/runtime-main.225fdb61.js"
+  }
+]);
